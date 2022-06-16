1f65655227fdd95191428ccfbe1a412bc37b3925
Civitasv <hscivitasv@gmail.com>
2022-06-16 14:52:09 +0800

    buffer line bug fix

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -17,7 +17,7 @@ bufferline.setup {
     buffer_close_icon = "",
     -- buffer_close_icon = '',
     modified_icon = "●",
-    close_icon = "",
+    close_icon= "",
     -- close_icon = '',
     left_trunc_marker = "",
     right_trunc_marker = "",
@@ -58,7 +58,7 @@ bufferline.setup {
     offsets = { { filetype = "NvimTree", text = "", padding = 1 } },
     show_buffer_icons = true,
     show_buffer_close_icons = true,
-    show_close_icon = true,
+    show_close_icon = false,
     show_tab_indicators = true,
     persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
     -- can also be a table containing 2 custom separators
