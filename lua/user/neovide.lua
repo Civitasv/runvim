54227b5c34dc7aa65ffc662b961b5f29731292dc
Civitasv <hscivitasv@gmail.com>
2022-06-16 10:30:50 +0800

    add neovide configuration

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,7 +1,9 @@
 vim.cmd([[
-set guifont=Source\ Code\ Pro:h16
+set guifont=Hack\ Nerd\ Font:h16
 set linespace=4
 
 let g:neovide_transparency=0.98
 let g:neovide_cursor_vfx_mode = "pixiedust"
+let g:neovide_cursor_animation_length=0.05
+let g:neovide_cursor_trail_length=0.2
 ]])
