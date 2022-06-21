86400ff0eb2d76c4945f5ae6e0687bf27a7166c1
Civitasv <hscivitasv@gmail.com>
2022-06-21 10:38:03 +0800

    change alpha message

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -7,36 +7,38 @@ local is_unix = vim.fn.has("unix") == 1
 local is_win32 = vim.fn.has("win32") == 1
 local configure_path
 if is_unix then
-  configure_path =  "~/.config/nvim/init.lua"
+	configure_path = "~/.config/nvim/init.lua"
 elseif is_win32 then
-  configure_path =  "~/AppData/Local/nvim/init.lua"
+	configure_path = "~/AppData/Local/nvim/init.lua"
 end
 local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {
-  [[                               __                ]],
-  [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
-  [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
-  [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
-  [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
-  [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+	[[                               __                ]],
+	[[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+	[[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+	[[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+	[[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+	[[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
 }
 dashboard.section.buttons.val = {
-  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-  dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-  dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-  dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("c", "  Configuration", ":e" .. configure_path ..  "<CR>"),
-  dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("c", "  Configuration", ":e" .. configure_path .. "<CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
 local function footer()
-  -- NOTE: requires the fortune-mod package to work
-  -- local handle = io.popen("fortune")
-  -- local fortune = handle:read("*a")
-  -- handle:close()
-  -- return fortune
-  return "@Civitasv"
+	local handle = io.popen("fortune")
+	if handle then
+		local fortune = handle:read("*a")
+		handle:close()
+		return fortune
+	else
+		return "@Civitasv"
+	end
 end
 
 dashboard.section.footer.val = footer()
