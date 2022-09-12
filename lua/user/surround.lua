de3d4159bf539689a402e5cb9d8efa6b7778cd50
Civitasv <hscivitasv@gmail.com>
2022-09-12 11:14:25 +0800

    update Surround.nvim

--- lua/user/surround.lua
+++ lua/user/surround.lua
@@ -10,31 +10,11 @@ surround.setup({
     delete = "ds",
     change = "cs",
   },
-  delimiters = {
-    pairs = {
-      ["("] = { "( ", " )" },
-      [")"] = { "(", ")" },
-      ["{"] = { "{ ", " }" },
-      ["}"] = { "{", "}" },
-      ["<"] = { "< ", " >" },
-      [">"] = { "<", ">" },
-      ["["] = { "[ ", " ]" },
-      ["]"] = { "[", "]" },
-    },
-    separators = {
-      ["'"] = { "'", "'" },
-      ['"'] = { '"', '"' },
-      ["`"] = { "`", "`" },
-    },
-    HTML = {
-      ["t"] = true, -- Use "t" for HTML-style mappings
-    },
-    aliases = {
-      ["a"] = ">", -- Single character aliases apply everywhere
-      ["b"] = ")",
-      ["B"] = "}",
-      ["r"] = "]",
-      ["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
-    },
-  }
+  aliases = {
+    ["a"] = ">", -- Single character aliases apply everywhere
+    ["b"] = ")",
+    ["B"] = "}",
+    ["r"] = "]",
+    ["q"] = { '"', "'", "`" }, -- Table aliases only apply for changes/deletions
+  },
 })
