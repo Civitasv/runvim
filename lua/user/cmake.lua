7079d9648382bb1abfcb948ffdd3ee8a59498ad3
Civitasv <hscivitasv@gmail.com>
2022-12-10 18:07:02 +0800

    update CMake Tools Configuration

--- lua/user/cmake.lua
+++ lua/user/cmake.lua
@@ -1,6 +1,7 @@
 require("cmake-tools").setup({
   cmake_command = "cmake",
-  cmake_build_directory = "build",
+  cmake_build_directory = "",
+  cmake_build_directory_prefix = "build/",
   cmake_build_type = "Debug",
   cmake_generate_options = { "-D", "CMAKE_EXPORT_COMPILE_COMMANDS=1" },
   cmake_build_options = {},
