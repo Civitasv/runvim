6f46b370357e5a4819067619a4cd1f2a28dab3cf
Civitasv <hscivitasv@gmail.com>
2023-01-15 19:56:22 +0800

    pcall for catppuccin

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -8,8 +8,12 @@
 --[[     dark_sidebar = "false" ]]
 --[[   } ]]
 --[[ ) ]]
+local status_ok, catppuccin = pcall(require, "catppuccin")
+if not status_ok then
+  return
+end
 
-require("catppuccin").setup({
+catppuccin.setup({
   flavour = "mocha", -- latte, frappe, macchiato, mocha
   background = { -- :h background
     light = "latte",
