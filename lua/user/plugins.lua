618ff6f988de4f371962718cbd1501322599e570
Civitasv <hscivitasv@gmail.com>
2023-01-24 19:26:31 +0800

    don't need bufferline

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,19 +1,16 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
+-- Automatically install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
     "git",
     "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
   })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
 end
+vim.opt.rtp:prepend(lazypath)
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
 vim.cmd([[
@@ -54,12 +51,12 @@ return packer.startup(function(use)
   use("goolord/alpha-nvim") -- a fast and fully customizable greeter for neovim
   use("antoinemadec/FixCursorHold.nvim") -- This is needed to fix lsp doc highlight
   use("kyazdani42/nvim-tree.lua") -- tree view
-  use("akinsho/bufferline.nvim")
-  use("moll/vim-bbye")
   use("nvim-lualine/lualine.nvim")
   use("akinsho/toggleterm.nvim")
   use("folke/which-key.nvim") -- vim which key
 
+  -- Bdelete command
+  use("moll/vim-bbye")
   -- Colorschemes
   use("projekt0n/github-nvim-theme")
   use({ "catppuccin/nvim", as = "catppuccin" })
