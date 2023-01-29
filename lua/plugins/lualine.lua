6ba4afd374e1a25084af6b3e07767575f86a8754
Civitasv <hscivitasv@gmail.com>
2023-01-29 20:53:26 +0800

    lualine catppuccin integration

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -47,10 +47,23 @@ return {
         magenta  = "#ae81ff",
         blue     = "#82aaff	",
         red      = "#ff5874",
+      },
+      catppuccin_mocha = {
+        bg       = "#1E1E2E",
+        fg       = "#CDD6F4",
+        yellow   = "#F9E2AF",
+        cyan     = "#7fdbca",
+        darkblue = "#89B4FA",
+        green    = "#A6E3A1",
+        orange   = "#e3d18a",
+        violet   = "#a9a1e1",
+        magenta  = "#ae81ff",
+        blue     = "#89B4FA",
+        red      = "#F38BA8",
       }
     }
 
-    colors = colors.normal;
+    colors = colors.catppuccin_mocha;
 
     local config = {
       options = {
