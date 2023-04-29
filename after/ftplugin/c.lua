25e7c63455e48d73ebe85a6464bc53389c37b15d
Civitasv <hscivitasv@gmail.com>
2023-04-29 15:53:17 +0800

    c filetype

---
+++ after/ftplugin/c.lua
@@ -0,0 +1,3 @@
+vim.opt.expandtab = false; -- convert tabs to spaces
+vim.opt.shiftwidth = 8; -- the number of spaces inserted for each indentation
+vim.opt.tabstop = 8; -- insert 2 spaces for a tab
