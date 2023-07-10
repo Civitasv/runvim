6502dd9f49936fd725d6e7cc2e6ff161af73b394
Civitasv <hscivitasv@gmail.com>
2023-07-10 09:23:16 +0800

    change alpha icon

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -25,12 +25,12 @@ return {
       [[ ━━━━━━━━━━━━━━━━━━━━━━━ ]]
     }
     dashboard.section.buttons.val = {
-      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
       dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-      dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
+      dashboard.button("t", "󰦨  Find text", ":Telescope live_grep <CR>"),
       dashboard.button("c", "  Configuration", ":e" .. configure_path .. "<CR>"),
-      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+      dashboard.button("q", "󰚩  Quit Neovim", ":qa<CR>"),
     }
 
     dashboard.section.footer.opts.hl = "Type"
