fdead202808683db79d44dc1a860265618b3707a
Civitasv <hscivitasv@gmail.com>
2023-12-25 17:30:35 +0800

    fix: a better chinese font

--- lua/config/neovide.lua
+++ lua/config/neovide.lua
@@ -1,6 +1,6 @@
 if vim.g.neovide then
-  vim.o.guifont = "Iosevka,Heiti SC:h16"
-  vim.g.neovide_scroll_animation_length = 0.3
+  vim.o.guifont = "Iosevka,STHeiti:h16"
+  vim.g.neovide_scroll_animation_length = 0.1
   vim.g.neovide_input_use_logo = true -- true on macOS
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_size = 0
