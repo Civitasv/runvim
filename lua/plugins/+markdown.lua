242afdb180c2cdb52bd3841fd92370e7919f9a56
Civitasv <hscivitasv@gmail.com>
2024-01-22 12:20:13 +0800

    add mdp.nvim

--- lua/plugins/+markdown.lua
+++ lua/plugins/+markdown.lua
@@ -1,15 +1,28 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  dependencies = {
-  },
-  build = "cd app && npm install",
-  ft = { "markdown" },
-  config = function()
-    vim.cmd([[
+  {
+    "iamcco/markdown-preview.nvim",
+    dependencies = {
+    },
+    build = "cd app && npm install",
+    ft = { "markdown" },
+    config = function()
+      vim.cmd([[
       let g:mkdp_theme = 'dark'
       let g:mkdp_auto_start=0
       let g:mkdp_auto_close=0
       let g:mkdp_refresh_slow=0
     ]])
-  end
+    end
+  },
+  {
+    dir = "~/.local/share/nvim/personal/mdp.nvim",
+    config = function()
+      require("mdp").setup(
+        {
+          pdfviewer = "Skim",
+          template = "eisvogel"
+        }
+      )
+    end
+  }
 }
