c9c31b7002dd733fb8efea994c0a0d16d7ce2eb1
Civitasv <hscivitasv@gmail.com>
2024-01-27 21:59:05 +0800

    add icon for toml

--- lua/plugins/+filetree.lua
+++ lua/plugins/+filetree.lua
@@ -2,6 +2,49 @@ return {
   "kyazdani42/nvim-tree.lua", -- tree view
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
+    require("nvim-web-devicons").setup {
+      -- your personnal icons can go here (to override)
+      -- you can specify color or cterm_color instead of specifying both of them
+      -- DevIcon will be appended to `name`
+      override = {
+        toml = {
+          icon = "",
+          color = "#428850",
+          cterm_color = "65",
+          name = "Toml"
+        }
+      },
+      -- globally enable different highlight colors per icon (default to true)
+      -- if set to false all icons will have the default icon's color
+      color_icons = true,
+      -- globally enable default icons (default to false)
+      -- will get overriden by `get_icons` option
+      default = true,
+      -- globally enable "strict" selection of icons - icon will be looked up in
+      -- different tables, first by filename, and if not found by extension; this
+      -- prevents cases when file doesn't have any extension but still gets some icon
+      -- because its name happened to match some extension (default to false)
+      strict = true,
+      -- same as `override` but specifically for overrides by filename
+      -- takes effect when `strict` is true
+      override_by_filename = {
+        [".gitignore"] = {
+          icon = "",
+          color = "#f1502f",
+          name = "Gitignore"
+        }
+      },
+      -- same as `override` but specifically for overrides by extension
+      -- takes effect when `strict` is true
+      override_by_extension = {
+        ["toml"] = {
+          icon = "",
+          color = "#81e043",
+          name = "Toml"
+        }
+      },
+    }
+
     -- following options are the default
     -- each of these are documented in `:help nvim-tree.OPTION_NAME`
     local nvim_tree = require("nvim-tree")
